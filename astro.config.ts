d595f893d8361c2e44806946358eef4d89f6c688
xexubonete <xexubonete@gmail.com>
2024-09-01 20:57:23 +0200

    Added vercel analytics

--- astro.config.ts
+++ astro.config.ts
@@ -1,16 +1,21 @@
 import { defineConfig } from 'astro/config'
 import react from '@astrojs/react'
 import tailwind from '@astrojs/tailwind'
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   site: process.env.CI
     ? 'https://astro-shadcn-ui-template.vercel.app'
     : 'http://localhost:4321',
-  integrations: [
+  integrations: [ 
     react(),
     tailwind({
       applyBaseStyles: false,
     }),
   ],
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: { enabled: true }
+  }),
 })
